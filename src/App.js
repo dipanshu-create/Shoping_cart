1eb8d104e9c7f7fe982fc74bb7dd6565e04699a2
Aswal <da185214@ncr.com>
2024-11-06 21:51:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Cart from './pages/cart';
 import Home from './pages/home';
+import Header from './components/header';
 
 function App() {
   return (
     <div>
-      <h1>Navbar</h1>
+      
+      <Header/>
       <Routes>
         <Route exact path="/" element={<Home/>}></Route>
         <Route path="cart" element={<Cart/>}></Route>
