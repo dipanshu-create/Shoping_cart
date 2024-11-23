accf2c539449470339f7ca1db079cea0dffbc735
Aswal <da185214@ncr.com>
2024-11-23 23:24:04 +0530

    adding cart page

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,7 +1,17 @@
-
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
 
 
 export default function Cart(){
+    const [totalCart,setTotalCart]=useState(0)
+
+    const {cart}=useSelector(state=>state)
+    
+    useEffect(()=>{
+        setTotalCart(cart.reduce((ace,curr)=>acc+curr.price))
+    },[cart])
+
+    console.log(state)
     return <div>Cart</div>
 }
 
